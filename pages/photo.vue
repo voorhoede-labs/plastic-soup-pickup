370c6989b323685f3c5c658f666d6bb573a4b693
Frank Warnaar <frank@voorhoede.nl>
2018-04-06 14:47:41 +0200

    feat(photo): fix for error when reinitializing firebase; only return best match label

--- pages/photo.vue
+++ pages/photo.vue
@@ -10,15 +10,20 @@
 </template>
 
 <script>
-import firebase from 'firebase'
-const app = firebase.initializeApp({
-  apiKey: "AIzaSyCTVK7d719KT2-ylcUAGZ4x0SY0Pc1nuVY",
-			authDomain: "pick-up-10-2e988.firebaseapp.com",
-			databaseURL: "https://pick-up-10-2e988.firebaseio.com",
-			projectId: "pick-up-10-2e988",
-			storageBucket: "gs://pick-up-10-2e988.appspot.com",
-			messagingSenderId: "317354700389"
-});
+import firebase from 'firebase';
+
+try {
+  firebase.initializeApp({
+    apiKey: "AIzaSyCTVK7d719KT2-ylcUAGZ4x0SY0Pc1nuVY",
+  			authDomain: "pick-up-10-2e988.firebaseapp.com",
+  			databaseURL: "https://pick-up-10-2e988.firebaseio.com",
+  			projectId: "pick-up-10-2e988",
+  			storageBucket: "gs://pick-up-10-2e988.appspot.com",
+  			messagingSenderId: "317354700389"
+  });
+} catch (err) {
+  console.log('Firebase already initialized');
+}
 
 export default {
   data() {
@@ -32,33 +37,31 @@ export default {
 
     fileRef
       .put(file)
-      .then(function(snapshot) {
-        console.log("uploaded a blob or file");
-        console.log(snapshot);
-
-        fetch('https://pick-up-10-api-xodrwwxowz.now.sh/api?userid=welcome12345&url=' + snapshot.downloadURL)
-          .then(function (response) {
-            console.log(response)
-          })
+      .then(snapshot => {
+        return fetch('https://pick-up-10-api-xodrwwxowz.now.sh/api?userid=welcome12345&url=' + snapshot.downloadURL)
       })
-      .catch(function(error) {
-        alert("error", error);
-      });
-  },
-  saveImage({ target }) {
-    const input = target;
-    if (input.files && input.files[0]) {
-      const reader = new FileReader();
-      const file = input.files[0];
+      .then(res => res.json())
+      .then(this.getLabel)
+      .then(console.log)
+      .catch(alert);
+    },
+    saveImage({ target }) {
+      const input = target;
+      if (input.files && input.files[0]) {
+        const reader = new FileReader();
+        const file = input.files[0];
 
-      reader.onload = event => {
-        this.src = event.target.result;
-      };
-      reader.readAsDataURL(file);
-      this.uploadToFirebase(file);
+        reader.onload = event => {
+          this.src = event.target.result;
+        };
+        reader.readAsDataURL(file);
+        this.uploadToFirebase(file);
+      }
+    },
+    getLabel(recognition) {
+      const bestGuesses = recognition.webDetection.bestGuessLabels;
+      return bestGuesses.length ? bestGuesses[0].label : 'No product recognized'
     }
-  }
   }
 }
 </script>
-
