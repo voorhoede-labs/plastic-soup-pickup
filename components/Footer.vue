f02836e2f575b35c4617141115910e4a60a493ac
bruna <brunadafonseca@gmail.com>
2018-04-06 14:35:23 +0200

    Add footer component

---
+++ components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="app-footer">
+    <engage-button />
+  </footer>
+</template>
+
+<style>
+  .app-footer {
+    width: 100%;
+    padding: 2em 0;
+    margin: 0 auto;
+  }
+</style>
+
+<script>
+import EngageButton from '~/components/EngageButton.vue'
+
+export default {
+  components: {
+    EngageButton,
+  }
+}
+</script>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,30 @@
 <template>
-  <section class="container">
-    <div>
-      <app-intro-video/>
-      <a href="/challenge" class="button">
-        let's go!
-      </a>
-      <introduction/>
-      <blockquote>I'm an awesome quote</blockquote>
-    </div>
-  </section>
+  <div>
+    <section class="container">
+      <div>
+        <app-intro-video/>
+        <a href="/challenge" class="button">
+          let's go!
+        </a>
+        <introduction/>
+        <blockquote>I'm an awesome quote</blockquote>
+      </div>
+    </section>
+    <footer/>
+  </div>
+  
 </template>
 
 <script>
 import AppIntroVideo from '~/components/AppIntroVideo.vue'
 import Introduction from '~/components/Introduction.vue'
+import Footer from '~/components/Footer.vue'
 
 export default {
   components: {
     AppIntroVideo,
-    Introduction
+    Introduction,
+    Footer,
   }
 }
 </script>
