3f6b3adda00c5262612aeef1d588a598d10b3da1
Casper Boutens <casper@voorhoede.nl>
2018-04-06 12:59:51 +0200

    feat(project): add global style

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,10 @@ module.exports = {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+  css: [
+    '~/css/fonts.css',
+    '~/css/main.css'
+  ],
   /*
   ** Customize the progress bar color
   */
