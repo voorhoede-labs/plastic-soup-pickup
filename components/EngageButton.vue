5e4908a82100319513f9e204b564af148d284fda
bruna <brunadafonseca@gmail.com>
2018-04-06 14:23:45 +0200

    Add introduction and engagebutton components

---
+++ components/EngageButton.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <nuxt-link to="/challenge" class="button">Start the challenge</nuxt-link>
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ components/Introduction.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="introduction">
+    <engage-button />
+  </section>
+</template>
+
+<style>
+  .introduction {
+    width: 100%;
+    background-color: #F26F69;
+  }
+</style>
+
+<script>
+import EngageButton from '~/components/EngageButton.vue'
+
+export default {
+  components: {
+    EngageButton,
+  }
+}
+</script>
+
