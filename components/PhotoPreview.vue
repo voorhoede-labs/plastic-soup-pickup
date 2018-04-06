c698bbd6972aaa71fb8fd46a649b95804834ba59
Frank Warnaar <frank@voorhoede.nl>
2018-04-06 18:05:06 +0200

    resolve conflicts

---
+++ components/PhotoPreview.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="photo-preview">
+    <img :src="src" class="image-preview"/>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    src: String,
+    entities: {
+      required: false,
+      type: Array,
+    },
+    bestGuess: {
+      required: false,
+      type: Object,
+    },
+  }
+}
+</script>
