c43ae7a1b8ce01ef88c3b985a47d507b592a88e2
Selwyn <talk@selwyn.cc>
2018-04-06 15:26:42 +0200

    Add initial challenge badges

---
+++ components/ChallengeBadge.vue
@@ -0,0 +1,30 @@
+<template>
+  <li>
+    <figure class="challenge-badge">
+      <img
+      v-bind:src="require(`~/assets/challenge-badges/${image}`)"
+      alt=""
+      width="150px"
+      height="150px"
+      >
+      <figcaption>Getting started</figcaption>
+      <p>25 points</p>
+    </figure>
+  </li>
+</template>
+
+<style>
+  .challenge-badge {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
+
+<script>
+export default {
+  props: {
+    image: String
+  }
+}
+</script>
