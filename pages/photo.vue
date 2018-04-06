f3e88e50fcbd96f25c58735a4fd5d116eede55c1
Anand Graves <anand@voorhoede.nl>
2018-04-06 13:23:26 +0200

    refactor: WIP, added markup, packages and logic for photo page

--- pages/photo.vue
+++ pages/photo.vue
@@ -1,11 +1,52 @@
+<script>
+import firebase from 'firebase'
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyCTVK7d719KT2-ylcUAGZ4x0SY0Pc1nuVY",
+			authDomain: "pick-up-10-2e988.firebaseapp.com",
+			databaseURL: "https://pick-up-10-2e988.firebaseio.com",
+			projectId: "pick-up-10-2e988",
+			storageBucket: "gs://pick-up-10-2e988.appspot.com",
+			messagingSenderId: "317354700389"
+});
+
+export default {
+  data() {
+    return { src: '' }
+  },
+  methods: {
+    saveImage() {
+      const input = this.refs.input;
+      if (input.files && input.files[0]) {
+        const reader = new FileReader();
+        const file = input.files[0];
+
+        reader.onload = event => {
+          this.src = event.target.result;
+        };
+
+        reader.readAsDataURL(file);
+
+        // uploadToFirebase(file);
+
+        // fetch("http://www.example.net", {
+        //   method: "POST",
+        //   body: file
+        // }).then(response => response.json());
+      }
+    }
+  }
+}
+</script>
+
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        take a picture
-      </h1>
-    </div>
-    <a href="/celebration">challenge completed</a>
-  </section>
+  <form method="post " action="">
+		<input @change="saveImage" data-input type="file" accept="image/*" capture ref="input">
+		<img ref="preview" :src="src" data-preview/>
+
+		<div>
+			<button type="submit">Upload Image</button>
+		</div>
+	</form>
 </template>
+
+
